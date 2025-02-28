72bf1e5306bfb3b10c140bf21ad2a0113632f75a
Daichi <daichi.bigshine777@gmail.com>
2025-02-28 15:12:23 +0900

    設定ページを作成

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SettingView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),  #(`/`) にアクセスしたときの処理
+    path("", IndexView.as_view(), name="index"),
+    path("setting", SettingView.as_view(), name="setting"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "main/index.html"
+    
+class SettingView(TemplateView):
+    template_name = "main/setting.html"
 
